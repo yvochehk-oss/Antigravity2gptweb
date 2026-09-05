33f807199432fb84d994a4858f3e5004eaec4c66
yvochehk-oss <yvochehk@gmail.com>
2026-09-05 13:03:14 +0800

    feat(vat): register completeness review router

--- source_code/0.1_税务管理/gtp_V1.0_FULL/01_当前完整系统_V1.0/chengdu_construction_tax_system_v1_0/app/routers/__init__.py
+++ source_code/0.1_税务管理/gtp_V1.0_FULL/01_当前完整系统_V1.0/chengdu_construction_tax_system_v1_0/app/routers/__init__.py
@@ -6,6 +6,7 @@ from .auth import router as auth_router
 from .canonical_ssot import router as canonical_ssot_router
 from .cockpit import router as cockpit_router
 from .collections import router as collections_router
+from .formal_vat_completeness import router as formal_vat_completeness_router
 from .formal_vat_readiness import router as formal_vat_readiness_router
 from .health_check import router as health_check_router
 from .legal_entity_operating_projection import router as legal_entity_operating_projection_router
@@ -53,6 +54,7 @@ ALL_ROUTERS = [
     phase4_accounting_router,
     v3_canonical_router,
     formal_vat_readiness_router,
+    formal_vat_completeness_router,
     tax_advisory_router,
     users_router,
     user_center_router,
